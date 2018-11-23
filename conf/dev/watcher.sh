0e693d9e44188657e9b444eac1c9d3b403eea2a7
Nicolas Talle <dev@nicolab.net>
2018-11-23 19:21:21 +0100

    [scripts] add exec

--- conf/dev/watcher.sh
+++ conf/dev/watcher.sh
@@ -3,14 +3,19 @@
 # app entrypoint
 dev_entrypoint="./main.go"
 
+# execute the entrypoint
+dev_exec() {
+  printf "\nExec $dev_entrypoint \n\n"
+  go run $dev_entrypoint
+}
+
 # simple watch
 dev_watch() {
-  go run $dev_entrypoint \
-  && reflex -g '*.go' go run $dev_entrypoint
+  dev_exec && reflex -g '**/*.go' -- dev_exec
 }
 
 # watch with Delve (Go debugger)
 dev_watch_debug() {
-  reflex --start-service -g '*.go' -- sh -c \
+  reflex --start-service -g '**/*.go' -- sh -c \
   "dlv debug $dev_entrypoint --headless=true --listen=0.0.0.0:2345 --api-version=2"
 }
